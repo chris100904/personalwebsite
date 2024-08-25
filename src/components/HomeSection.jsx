074269f0299bf0d73396b2cb6474650d00bee5cc
chris100904 <73714518+chris100904@users.noreply.github.com>
2024-08-24 23:52:51 -0400

    minor bug

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -261,10 +261,9 @@ const HomeSection = ({ toggleModal }) => {
               {videos.map((videoSrc, index) => (
                 <div key={index}>
                   <iframe
-                    width="100%"
+                    width="80%"
                     height="400"
                     src={activeIndex === index ? videoSrc : ""}
-                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                   ></iframe>
